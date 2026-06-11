808265a71f99071b089f842fee1faf0022844d50
chenyanhui <1574475923@qq.com>
2026-06-11 11:40:15 +0800

    refactor: 删除风险评估模块相关代码

--- src/main/java/com/never_give_up/automation/demo1/dto/request/ParseMaterialRequest.java
+++
@@ -1,9 +0,0 @@
-package com.never_give_up.automation.demo1.dto.request;
-
-import lombok.Data;
-
-@Data
-public class ParseMaterialRequest {
-    private Boolean forceOcr;
-    private Integer maxChars;
-}
